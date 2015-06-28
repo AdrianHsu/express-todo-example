641c5213e49c758ec9b7375916f0c4d5167486a3
AdrianHsu <adrianhsu1995@gmail.com>
2015-06-29 02:15:13 +0800

    first commit on express.js

--- README.md
+++
@@ -1,4 +0,0 @@
-# Express Todo Example
-By Adrian Hsu 20150629
-
-Source code for the tutorial [Write a todo list with Express and MongoDB](http://dreamerslab.com/blog/en/write-a-todo-list-with-express-and-mongodb/)
--- app.js
+++ app.js
@@ -1,53 +1,60 @@
-/**
- * Module dependencies.
- */
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-// mongoose setup
-require( './db' );
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-var express        = require( 'express' );
-var http           = require( 'http' );
-var path           = require( 'path' );
-var engine         = require( 'ejs-locals' );
-var favicon        = require( 'serve-favicon' );
-var cookieParser   = require( 'cookie-parser' );
-var bodyParser     = require( 'body-parser' );
-var methodOverride = require( 'method-override' );
-var logger         = require( 'morgan' );
-var errorHandler   = require( 'errorhandler' );
-var static         = require( 'serve-static' );
+var app = express();
 
-var app    = express();
-var routes = require( './routes' );
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// all environments
-app.set( 'port', process.env.PORT || 3001 );
-app.engine( 'ejs', engine );
-app.set( 'views', path.join( __dirname, 'views' ));
-app.set( 'view engine', 'ejs' );
-app.use( favicon( __dirname + '/public/favicon.ico' ));
-app.use( logger( 'dev' ));
-app.use( methodOverride());
-app.use( cookieParser());
-app.use( bodyParser.json());
-app.use( bodyParser.urlencoded({ extended : true }));
+app.use('/', routes);
+app.use('/users', users);
 
-// Routes
-app.use( routes.current_user );
-app.get(  '/',            routes.index );
-app.post( '/create',      routes.create );
-app.get(  '/destroy/:id', routes.destroy );
-app.get(  '/edit/:id',    routes.edit );
-app.post( '/update/:id',  routes.update );
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-app.use( static( path.join( __dirname, 'public' )));
+// error handlers
 
-// development only
-if( 'development' == app.get( 'env' )){
-  app.use( errorHandler());
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
 }
 
-http.createServer( app ).listen( app.get( 'port' ), function (){
-  console.log( 'Express server listening on port ' + app.get( 'port' ));
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
+
+module.exports = app;
